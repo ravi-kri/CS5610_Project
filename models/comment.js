1e60e8d6fa56b2c482c3d7b9c628222a8ce76499
Harsh Modi <modi.ha@husky.neu.edu>
2019-06-17 15:50:14 -0400

    reformat

--- models/comment.js
+++ models/comment.js
@@ -2,15 +2,15 @@ var mongoose = require("mongoose");
 
 var commentSchema = mongoose.Schema({
     text: String,
-    createdAt: { type: Date, default: Date.now },
+    createdAt: {type: Date, default: Date.now},
     author:
         {
-            id :
+            id:
                 {
-                    type : mongoose.Schema.Types.ObjectId,
-                    ref : "User"
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User"
                 },
-            username : String
+            username: String
         }
 });
 
